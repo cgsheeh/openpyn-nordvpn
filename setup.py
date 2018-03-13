2897756311e5cae6a0e97a0e9536addd9032e1dc
Connor Sheehan <sheehan@mozilla.com>
2018-03-13 00:15:04 -0400

    Add tabulate dependency

--- setup.py
+++ setup.py
@@ -26,7 +26,9 @@ setuptools.setup(
     keywords=[
         'openvpn wrapper', 'nordvpn', 'nordvpn client', 'secure vpn',
         'vpn wrapper', 'private vpn', 'privacy'],
-    install_requires=['requests', 'colorama'],
+    install_requires=['requests', 
+                      'colorama', 
+                      'tabulate==0.8.2'],
     platforms=['GNU/Linux', 'Ubuntu', 'Debian', 'Kali', 'CentOS', 'Arch', 'Fedora'],
     packages=setuptools.find_packages(),
     entry_points={
